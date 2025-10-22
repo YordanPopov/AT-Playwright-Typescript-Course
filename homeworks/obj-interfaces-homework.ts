d0112131662a95c57313f1d6acda34a99443fdb2
YordanPopov <jordanpopov190@gmail.com>
2025-10-22 19:25:15 +0300

    add obj-interfaces-homework.ts file

---
+++ homeworks/obj-interfaces-homework.ts
@@ -0,0 +1,34 @@
+interface Person {
+  name: string;
+  lastName: string;
+  age: number;
+  email: string;
+  phoneNumber: number;
+}
+
+interface PersonInfo extends Person {
+  country?: string;
+  greeting?: () => string;
+}
+
+const person: Person = {
+  name: "John",
+  lastName: "Smith",
+  age: 33,
+  email: "m_example@email.com",
+  phoneNumber: 1234567890,
+};
+
+const person2: PersonInfo = {
+  name: "Anna",
+  lastName: "Doe",
+  age: 30,
+  email: "f_example@email.com",
+  phoneNumber: 9876543210,
+  country: "USA",
+  greeting: function (): string {
+    return `Welcome, ${person2.name} ${person2.lastName}!`;
+  },
+};
+
+console.log(person2.greeting?.());
